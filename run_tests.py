755f9c2fbc319a0115ea1a6208746ed77b3a10fc
ryanc16 <rconklin1994@yahoo.com>
2021-08-15 20:46:37 -0400

    Adding Colors tests

--- run_tests.py
+++ run_tests.py
@@ -3,11 +3,13 @@ from src.test.framework.test_runner import TestRunner
 from src.test.utils.time_functions_test import TimeFunctionsTest
 from src.test.utils.color_functions_test import ColorFunctionsTest
 from src.test.utils.rgb_color_test import RgbColorTest
+from src.test.utils.colors_test import ColorsTest
 
 if __name__ == "__main__":
 	tests = [
 		TimeFunctionsTest,
 		ColorFunctionsTest,
-		RgbColorTest
+		RgbColorTest,
+		ColorsTest
 	]
 	TestRunner(tests).run()
