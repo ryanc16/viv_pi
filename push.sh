99c2c711ce3a781e9221015deb322ff77c13f5b8
ryanc16 <rconklin1994@yahoo.com>
2021-08-16 22:53:43 -0400

    Update the push script to point to the project root to allow pushing the start.py and config.py to the project root

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 pi_user=$(cat ./keys/pi_user)
 pi_ip=$(cat ./keys/pi_ip)
-pi_dir="~/Projects/viv_pi/src/main"
-scp -i ./keys/pi_rsa -rp src/main/** "$pi_user@$pi_ip:$pi_dir/"
+pi_dir="~/Projects/viv_pi"
+scp -i ./keys/pi_rsa -rp src/main/** "$pi_user@$pi_ip:$pi_dir/src/main/"
+scp -i ./keys/pi_rsa start.py config.py "$pi_user@$pi_ip:$pi_dir/"
