4536713b091f7c1454f23bff20cf99f6028a3d88
ryanc16 <rconklin1994@yahoo.com>
2021-08-16 22:51:50 -0400

    Update the main.py to use system config and set up lighting controller. Update start.py to pass the configuration into the Main. Add bash script to start system.

--- src/main/main.py
+++ src/main/main.py
@@ -1,20 +1,23 @@
 from datetime import datetime
 from time import sleep
 
-from src.main.config import SystemConfig
+from src.main.system_config import SystemConfig
 from src.main.controllers.lighting.lighting_controller import LightingController
 
-config: SystemConfig = SystemConfig
 class Main:
 
-	def __init__(self):
+	def __init__(self, systemConfig: SystemConfig):
 		print("viv-pi started at", datetime.now())
-		if config.lighting.ENABLED:
-			self.lightingController = LightingController()
-			self.lightingController.start()
+		self.systemConfig = systemConfig
 
-	def onUpdate(self):
+		if systemConfig.lighting.ENABLED:
+			print("starting: LightingController")
+			self.lightingController = LightingController(systemConfig.lighting)
+			self.lightingController.start()
+		
 		while True:
+			self.onUpdate()
 			sleep(1)
 
-Main().onUpdate()
+	def onUpdate(self):
+		pass
--- start.py
+++ start.py
@@ -1,1 +1,5 @@
-import src.main.main
+from src.main.main import Main
+from importlib import import_module
+
+config_module = import_module('config')
+Main(config_module.SYSTEM_CONFIG)
