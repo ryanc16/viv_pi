3f47784d43a194dbd123a4882d2070cec2464451
ryanc16 <rconklin1994@yahoo.com>
2021-08-24 13:42:16 -0400

    Add Mock and Spy to mocks for whole class mocking

--- src/test/framework/mocks.py
+++ src/test/framework/mocks.py
@@ -1,7 +1,8 @@
+from inspect import getmembers, isfunction, ismethod
 from src.test.framework.test_runner import TestRunner
 
 
-class BaseMock:
+class BaseMockFn:
 
   def __init__(self, class_instance: object, method: str):
     self._original_class = class_instance
@@ -15,7 +16,7 @@ class BaseMock:
   def restore(self):
     setattr(self._original_class, self._original_method, self._original_func)
 
-class MockFn(BaseMock):
+class MockFn(BaseMockFn):
 
   def __init__(self, class_instance: object, method: str):
     super().__init__(class_instance, method)
@@ -32,6 +33,9 @@ class MockFn(BaseMock):
     self._calls.append((args, returnVal))
     return returnVal
 
+  def __call__(self, *args):
+    return self._invoke(*args)
+
   def thenDo(self, fn):
     self._mockAction = lambda *args: fn.__call__()
     return self
@@ -39,7 +43,8 @@ class MockFn(BaseMock):
   def thenReturn(self, value):
     self._returnVal = value
 
-class SpyFn(BaseMock):
+class SpyFn(BaseMockFn):
+
   def __init__(self, class_instance: object, method: str):
     super().__init__(class_instance, method)
     setattr(class_instance, method, lambda *args: self._invoke(*args))
@@ -49,41 +54,90 @@ class SpyFn(BaseMock):
     self._calls.append((args, returnVal))
     return returnVal
 
-class MockAssertion:
-  def __init__(self, mock: BaseMock):
+  def __call__(self, *args):
+    return self._invoke(*args)
+
+class BaseMock:
+  def __init__(self, class_instance: object):
+    self.mocks = []
+    self._original_class = class_instance
+
+  def reset(self):
+    for m in self.mocks:
+      m.reset()
+  
+  def restore(self):
+    for m in self.mocks:
+      m.restore()
+
+class Mock(BaseMock):
+
+  def __init__(self, class_instance: object):
+    super().__init__(class_instance)
+    self.mocks = list(map(lambda fn: mockMethod(class_instance, fn[0]), getmembers(class_instance, lambda member: isfunction(member) or ismethod(member))))
+    for m in self.mocks:
+      setattr(self, m._original_method, m)
+
+  def when(self, method: str) -> MockFn:
+    return list(filter(lambda m: m._original_method == method, self.mocks))[0]
+
+class Spy(BaseMock):
+
+  def __init__(self, class_instance: object):
+    super().__init__(class_instance)
+    self.mocks = list(map(lambda fn: spyMethod(class_instance, fn[0]), getmembers(class_instance, lambda member: isfunction(member) or ismethod(member))))
+    for m in self.mocks:
+      setattr(self, m._original_method, m)
+
+class MockFnAssertion:
+  def __init__(self, mock: BaseMockFn):
     self.mock = mock
 
   def toHaveBeenCalled(self) -> bool:
-    assert len(self.mock._calls) > 0, f"Expected {self.mock.original_class.__name__}.{self.mock.original_method} to have been called"
+    assert len(self.mock._calls) > 0, f"Expected {self.mock._original_class.__name__}.{self.mock._original_method} to have been called"
 
   def toHaveBeenCalledTimes(self, times: int) -> bool:
-    assert len(self.mock._calls) == times, f"Expected {self.mock.original_class.__name__}.{self.mock.original_method} to have been called {times} times, but was actually called {len(self.mock._calls)} times"
+    assert len(self.mock._calls) == times, f"Expected {self.mock._original_class.__name__}.{self.mock._original_method} to have been called {times} times, but was actually called {len(self.mock._calls)} times"
   
   def toHaveBeenCalledWith(self, arg) -> bool:
     calls = list(map(lambda invocation: invocation[0], self.mock._calls))
-    assert arg in calls, f"Expected {self.mock.original_class.__name__}.{self.mock.original_method} to have been called with {arg}.\nActual calls include {calls}"
+    assert arg in calls, f"Expected {self.mock._original_class.__name__}.{self.mock._original_method} to have been called with {arg}.\nActual calls include {calls}"
 
   def toHaveLastBeenCalledWith(self, arg) -> bool:
     call = list(map(lambda invocation: invocation[0], self.mock._calls))[len(self.mock._calls)-1]
-    assert call == arg, f"Expected {self.mock.original_class.__name__}.{self.mock.original_method} to have last been called with {arg}.\nActual last call was {call}"
+    assert call == arg, f"Expected {self.mock._original_class.__name__}.{self.mock._original_method} to have last been called with {arg}.\nActual last call was {call}"
 
   def toHaveReturned(self, value) -> bool:
     calls = list(map(lambda invocation: invocation[1], self.mock._calls))
-    assert value in calls, f"Expected {self.mock.original_class.__name__}.{self.mock.original_method} to have returned {value}.\nActual returned values include {calls}"
+    assert value in calls, f"Expected {self.mock._original_class.__name__}.{self.mock._original_method} to have returned {value}.\nActual returned values include {calls}"
 
   def toHaveLastReturned(self, value) -> bool:
     call = list(map(lambda invocation: invocation[1], self.mock._calls))[len(self.mock._calls)-1]
-    assert call == value, f"Expected {self.mock.original_class.__name__}.{self.mock.original_method} to have last returned {value}.\nActual last returned value was {call}"
+    assert call == value, f"Expected {self.mock._original_class.__name__}.{self.mock._original_method} to have last returned {value}.\nActual last returned value was {call}"
 
-def mock(class_instance: object, method: str):
-  mock = MockFn(class_instance, method)
-  TestRunner.mocks.append(mock)
-  return mock
+def mockMethod(class_instance: object, method: str) -> MockFn:
+  m = MockFn(class_instance, method)
+  TestRunner.mocks.append(m)
+  return m
 
-def spyOn(class_instance: object, method: str):
-  spy = SpyFn(class_instance, method)
-  TestRunner.mocks.append(spy)
-  return spy
+def mock(class_instance: object) -> Mock:
+  m = Mock(class_instance)
+  TestRunner.mocks.append(m)
+  return m
 
-def assertMock(mock) -> MockAssertion:
-  return MockAssertion(mock)
+def spyMethod(class_instance: object, method: str) -> SpyFn:
+  s = SpyFn(class_instance, method)
+  TestRunner.mocks.append(s)
+  return s
+
+def spy(class_instance: object) -> Spy:
+  s = Spy(class_instance)
+  TestRunner.mocks.append(s)
+  return s
+
+def assertMock(mock: BaseMock or BaseMockFn, method: str = None) -> MockFnAssertion:
+  if (issubclass(type(mock), BaseMock) and method is not None):
+    mockFn = list(filter(lambda m: m._original_method == method, mock.mocks))[0]
+    return MockFnAssertion(mockFn)
+  else:
+    return MockFnAssertion(mock)
