46fcb0dca8821af0509c6aee688ba2199ab5f93e
ryanc16 <rconklin1994@yahoo.com>
2021-09-11 20:38:41 -0400

    Change main and start to have a start and stop process using threading and events

--- src/main/main.py
+++ src/main/main.py
@@ -1,23 +1,39 @@
 from datetime import datetime
-from time import sleep
+import threading
+from typing import List
 
-from src.main.system_config import SystemConfig
-from src.main.controllers.lighting.lighting_controller import LightingController
+from src.main.controllers.controller import Controller
+from src.main.system_config import ControllerConfig, SystemConfig
 
 class Main:
 
-	def __init__(self, systemConfig: SystemConfig):
-		print("viv-pi started at", datetime.now())
-		self.systemConfig = systemConfig
+  def __init__(self, systemConfig: SystemConfig):
+    self.systemConfig = systemConfig
+    self.controllers: List[Controller] = []
 
-		if systemConfig.lighting.ENABLED:
-			print("starting: LightingController")
-			self.lightingController = LightingController(systemConfig.lighting)
-			self.lightingController.start()
-		
-		while True:
-			self.onUpdate()
-			sleep(1)
+  def onStart(self):
+    print("viv-pi started at", datetime.now())
+    for ctrlConfig in self.systemConfig.controllers:
+      self._startController(ctrlConfig)
+    
+    self.exit = threading.Event()
+    while not self.exit.is_set():
+      self.onUpdate()
+      self.exit.wait(1)
 
-	def onUpdate(self):
-		pass
+  def onStop(self):
+    self.exit.set()
+    for controller in self.controllers:
+      print(f"Stopping: {controller.__class__.__name__}")
+      controller.stop()
+    print("viv-pi stopped at", datetime.now())
+
+  def onUpdate(self):
+    pass
+
+  def _startController(self, ctrlConfig: ControllerConfig):
+    if ctrlConfig.config.ENABLED:
+      print(f"Starting: {ctrlConfig.type.__name__}")
+      controller: Controller = ctrlConfig.type(ctrlConfig.config)
+      controller.start()
+      self.controllers.append(controller)
--- start.py
+++ start.py
@@ -2,4 +2,9 @@ from src.main.main import Main
 from importlib import import_module
 
 config_module = import_module('config')
-Main(config_module.SYSTEM_CONFIG)
+app = Main(config_module.SYSTEM_CONFIG)
+try:
+  app.onStart()
+except KeyboardInterrupt:
+  print()
+  app.onStop()
