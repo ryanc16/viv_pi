352f78c477ecee64ad26291222fcbf198b598589
ryanc16 <rconklin1994@yahoo.com>
2021-10-03 15:34:15 -0400

    delete dev-setup as it was essentially needed for normal setup as well. Remove windows .bat setup

--- scripts/install.bat
+++
@@ -1,9 +0,0 @@
-echo off
-echo "configuring for windows"
-if not exist .venv\ (
-  echo "create local venv"
-  mkdir .venv
-  type NUL > .venv\.gitkeep
-)
-echo "activating and installing requirements"
-pipenv install
