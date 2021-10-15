6929988f138a1d18a8e85c04acad3f61abeec875
ryanc16 <rconklin1994@yahoo.com>
2021-10-15 02:45:57 -0400

    Add coverage for unit tests

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ from assert4py.test_runner import TestRunner, TestRunnerConfig
 if __name__ == "__main__":
 	config: TestRunnerConfig = TestRunnerConfig(
 		fileGlob = "tests/**/*_test.py",
-		verbosity="SUMMARY" # SUMMARY, INFO, VERBOSE
+		verbosity="INFO" # SUMMARY, INFO, VERBOSE
 	)
 	result = TestRunner(config).run()
 	sys.exit(0 if result else 1)
